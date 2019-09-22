6c524739c8e5a526af1e98ea76050d121a926aed
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-09-22 19:39:37 +0400

    Create joke.js

---
+++ commands/joke.js
@@ -0,0 +1,18 @@
+
+const { MessageEmbed } = require("discord.js");
+module.exports.run = async (client, message, args) => {
+const Joker = require("blague.xyz");
+const joker = new Joker({
+    token: "ESC.CqoMAm-G50-rjM2.52L1GMYU.r1qovog_n6BZtFqMbzwaL6M.m1q9rVaY_yr"
+});
+ 
+// Obtenir une blague aléatoire
+joker.daily().then((joke) => {
+const em = new MessageEmbed()
+.setTitle("__Blague du jour__")
+.setDescription(`${joke.question}\n||${joke.answer}||`)
+}
+module.exports.config = {
+name: "joke",
+aliases: []
+}
