988246f049aec9950278e6de17349f439c8eb089
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-09-23 20:34:10 +0400

    Create glitch.js

---
+++ commands/glitch.js
@@ -0,0 +1,15 @@
+module.exports.run = async (client,  message, args) => {
+let user = message.mentions.users.first() || message.author;
+let avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=2048`;
+ 
+const ameClient = require("amethyse-api")
+ const ameApi = new ameClient("ff41a095bc493607d2865295c74cf72329fa9681b5fb57c08bae6493d550d7bc3c0433508e04dfd3f2c569d888b8ae33e20529fb1489b6b2e25f92226b6ef1d2")
+let image = ameApi.generate("glitch", { 	 "url" : avatar }) 
+//console.log(await image)
+ message.channel.send(await image)
+}
+module.exports.config = {
+name: "glitch",
+aliases: []
+}
+ 
