b90cccb0eb022f3761c07730859c4b4c7ded2e38
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-09-25 11:06:20 +0400

    Update glitch.js

--- commands/glitch.js
+++ commands/glitch.js
@@ -3,10 +3,15 @@ let user = message.mentions.users.first() || message.author;
 let avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=2048`;
  
 const ameClient = require("amethyste-api")
- const ameApi = new ameClient("ff41a095bc493607d2865295c74cf72329fa9681b5fb57c08bae6493d550d7bc3c0433508e04dfd3f2c569d888b8ae33e20529fb1489b6b2e25f92226b6ef1d2")
+ const ameApi = new ameClient("token ")a
 let image = ameApi.generate("glitch", { 	 "url" : avatar }) .then(image => {
       console.log(image)
-message.channel.send(image)
+message.channel.send({
+   files: [{
+       attachment: image, 
+       name: "glitch.png" 
+   }] 
+})
     }).catch(err => {
       throw err;
     })
